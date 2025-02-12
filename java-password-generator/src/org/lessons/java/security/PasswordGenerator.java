09f01493ff0fa444472eee7cbdd9bb6f486a7c85
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-12 16:32:05 +0100

    create variables and import scanner

--- java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
+++ java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,21 @@
 package org.lessons.java.security;
 
+//importo lo scanner
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) throws Exception {
 
+        // creo una variabile per il nome
+        String nome = "";
+
+        // creo una variabile per il cognome
+        String cognome = "";
+
+        // creo una variabile per il colore preferito
+        String colorePreferito = "";
+
+        // creo una variabile per la data di nascita
+        int dataNascita;
     }
 }
