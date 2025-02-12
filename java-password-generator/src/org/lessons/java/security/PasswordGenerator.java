26b5c792bf4a475dac4b1d5f77037e336d1203e5
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-12 16:40:43 +0100

    create inputs for generate the password

--- java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
+++ java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
@@ -17,5 +17,28 @@ public class PasswordGenerator {
 
         // creo una variabile per la data di nascita
         int dataNascita;
+
+        Scanner in = new Scanner(System.in);
+
+        // creo l'input per il nome dell'utente
+        System.out.println("Qual è il tuo nome?");
+        nome = in.nextLine();
+
+        // creo l'input per il cognome dell'utente
+        System.out.println("Qual è il tuo cognome?");
+        cognome = in.nextLine();
+
+        // creo l'input per il colore preferito dell'utente
+        System.out.println("Qual è il tuo colore preferito?");
+        colorePreferito = in.nextLine();
+
+        // creo l'input per la data di nascita dell'utente
+        System.out.println("Qual è la tua data di nascita?");
+        dataNascita = in.nextInt();
+        in.nextLine();
+
+        // stampo a schermo la password
+        System.out.println("La tua pa");
+
     }
 }
